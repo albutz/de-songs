802267d1628bff718f3b2e9e97d211e73016cd9e
Alexander Butz <alex.butz@gmail.com>
2022-06-09 19:01:21 +0200

    Switch to manual start of Postgres container

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 """Data onboarding for the million songs dataset with Docker, SQLAlchemy and Postgres."""
 import logging
 
-from python_on_whales import DockerClient
+from python_on_whales import docker
 from sqlalchemy import create_engine, inspect
 
 from etl import run_initial_pipeline
-from setup import Docker
 from tables import metadata
 
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
 
-    docker_client = DockerClient(compose_files=["docker-compose.yml"])
-
-    with Docker(docker_client) as container:
+    # Check that container is running
+    container_names = [container.name for container in docker.ps()]
+    if "sparkify" in container_names:
 
         engine = create_engine(
             "postgresql+psycopg2://postgres:postgres@localhost:6543/sparkifydb", future=True
@@ -27,8 +26,17 @@ if __name__ == "__main__":
         if not all([tbl in inspector.get_table_names() for tbl in ["artists", "songs"]]):
             with engine.begin() as conn:
                 metadata.create_all(conn)
+            logging.info("Schema created.")
+        else:
+            logging.info("Schema already exists.")
 
         # ETL pipeline
         run_initial_pipeline(engine)
+        logging.info("Initial ETL pipeline successfully run.")
 
         engine.dispose()
+
+        logging.warning("Make sure to shut down the Postgres container.")
+
+    else:
+        logging.error("No Postgres container running.")
